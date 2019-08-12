692e9ed59b367a5d69365091ec5d97d9c89413e2
Douglas Johnson <doug@thoughtcatalog.com>
2019-08-12 16:22:41 -0400

    add theme support function

--- functions.php
+++ functions.php
@@ -5,6 +5,28 @@
  * @package Cata
  */
 
+if ( ! function_exists( 'cata_after_setup_theme' ) ) :
+	/**
+	 * After Setup Theme
+	 */
+	function cata_after_setup_theme() {
+
+		$html5_options = array(
+			'caption',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'search-form',
+		);
+
+		add_theme_support( 'html5', $html5_options );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
+
+	}
+endif;
+add_action( 'after_setup_theme', 'cata_after_setup_theme' );
+
 /**
  * Autoload
  * Register our autoloader.
