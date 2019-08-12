6ebf80772a919082735a9b5961dd7782991d29ba
Douglas Johnson <doug@thoughtcatalog.com>
2019-08-12 11:19:39 -0400

    add basic template parts, mirroring _s

--- index.php
+++ index.php
@@ -6,4 +6,13 @@
  */
 
 get_header();
+?>
+<div class="site-content" id="content">
+	<div class="content-area" id="primary">
+		<main class="site-main" id="main">
+		</main>
+	</div>
+	<?php get_sidebar(); ?>
+</div>
+<?php
 get_footer();
---
+++ template-parts/document-open.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Document Open
+ * 
+ * @package Cata
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div class="site" id="page">
