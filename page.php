ab9ec480082d4fee2aae6f9c96a7282a82fe0d99
Douglas Johnson <doug@thoughtcatalog.com>
2019-08-13 15:35:45 -0400

    add single and page templates

---
+++ page.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * Page
+ * 
+ * @package Cata
+ */
+
+get_header();
+?>
+<div class="site__content" id="content">
+	<section class="site__primary area--content" id="primary">
+		<main class="site__main" id="main">
+		<?php
+		while ( have_posts() ) :
+
+			the_post();
+
+			get_template_part( 'template-parts/content/content', get_post_type() );
+			
+			the_post_navigation();
+
+			if ( comments_open() ) :
+				comments_template();
+			endif;
+
+		endwhile;
+		?>
+		</main>
+	</section>
+	<?php get_sidebar(); ?>
+</div>
+<?php
+get_footer();
