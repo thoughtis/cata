d0056d26feee79cabab1a4385991b7e4f7450c4a
Douglas Johnson <doug@thoughtcatalog.com>
2022-09-07 14:29:19 -0400

    add streaming button style

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -10,6 +10,7 @@ namespace Cata;
 new Blocks\Core_Columns();
 new Blocks\Experimental\Border();
 new Blocks\Experimental\Font_Family();
+new Blocks\Styles\Streaming();
 new Custom_Formats\Mark();
 new Enqueue_Assets();
 new Images\Block_Image_Sizes\Setter(
