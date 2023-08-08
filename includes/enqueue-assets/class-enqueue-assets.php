279e2285bcdce27f7c98b31faa881b2d47ddd2c1
Jessica Townsend <jessica@thoughtcatalog.com>
2023-08-08 07:37:48 -0700

    enqueue reset styles

--- includes/enqueue-assets/class-enqueue-assets.php
+++ includes/enqueue-assets/class-enqueue-assets.php
@@ -18,10 +18,18 @@ if ( ! class_exists( 'Cata\Enqueue_Assets' ) ) :
 		 * Construct
 		 */
 		public function __construct() {
+			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_styles_reset_blocking' ), 5 );
 			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_styles_blocking' ) );
 			add_action( 'wp_body_open', array( __CLASS__, 'enqueue_styles_nonblocking' ) );
 			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
 			add_filter( 'script_loader_tag', array( __CLASS__, 'use_module_type' ), 10, 3 );
+		}
+
+		/** 
+		 * Enqueue Blocking Reset/Normalize Styles
+		 */
+		public static function enqueue_styles_reset_blocking() : void {
+			wp_enqueue_style( 'cata-blocking', get_template_directory_uri() . '/assets/dist/css/reset-blocking.css', array( 'wp-block-library' ), wp_get_theme( 'cata' )->get( 'Version' ), 'screen' );
 		}
 
 		/**
