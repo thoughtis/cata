ad5ac2d5e665813acee29071042f91144f849136
Jessica Townsend <jessica@thoughtcatalog.com>
2024-03-11 12:23:07 -0700

    remove theme support for woocommerce

--- functions.php
+++ functions.php
@@ -36,17 +36,6 @@ if ( ! function_exists( 'cata_after_setup_theme' ) ) :
 		add_theme_support( 'html5', $html5_options );
 		add_theme_support( 'responsive-embeds' );
 		add_theme_support( 'title-tag' );
-
-		/**
-		 * Theme Supports Woocommerce
-		 * 
-		 * @link https://github.com/woocommerce/woocommerce/wiki/Declaring-WooCommerce-support-in-themes
-		 */
-		add_theme_support( 'woocommerce' );
-		add_theme_support( 'wc-product-gallery-lightbox' );
-		add_theme_support( 'wc-product-gallery-slider' );
-		
-		remove_theme_support( 'core-block-patterns' );
 	}
 endif;
 add_action( 'after_setup_theme', 'cata_after_setup_theme' );
